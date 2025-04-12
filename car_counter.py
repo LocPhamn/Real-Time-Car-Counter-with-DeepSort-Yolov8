e838ac9d7eb7d26f460c55233fceca9523dab857
LocPhamn <jasonpham20203@gmail.com>
2025-04-12 21:29:17 +0700

    remove comment

--- car_counter.py
+++ car_counter.py
@@ -83,59 +83,6 @@ while True:
     cv2.imshow("Image", img)
     if cv2.waitKey(1) == ord("q"):
         break
-        # boxes = r.boxes
-        # for box in boxes:
-        #     # Bounding Box
-        #     x1, y1, x2, y2 = box.xyxy[0]
-        #     x1, y1, x2, y2 = float(x1), float(y1), float(x2), float(y2)
-        #     w, h = x2 - x1, y2 - y1
-        #
-        #     # Confidence
-        #     conf = math.ceil((box.conf[0] * 100)) / 100
-        #     # Class Name
-        #     cls = int(box.cls[0])
-        #     currentClass = classNames[cls]
-        #
-        #     if currentClass in ["car", "truck", "bus", "motorbike"] and conf > 0.3:
-        #
-        #         # object detect, show label and confident
-        #         cvzone.putTextRect(img, '{} {:0.2f}'.format(currentClass,conf), (max(0, x1), max(35, y1)),
-        #                            scale=1, thickness=1, offset=3)
-        #         cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
-        #
-        #         currentArray = np.array([x1, y1, x2, y2, conf])
-        #         detections = np.vstack((detections, currentArray))
 
-
-
-
-    # resultsTracker = tracker.update(detections)
-    #
-    # for result in resultsTracker:
-    #     x1, y1, x2, y2, id = result
-    #     x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-    #     print(result)
-    #     w, h = x2 - x1, y2 - y1
-    #
-    #     # show tracking id
-    #     cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 255))
-    #     cvzone.putTextRect(img, ' {}'.format(int(id)), (max(0, x1), max(35, y1)),
-    #                        scale=2, thickness=3, offset=10)
-    #
-    #     cx, cy = x1 + w // 2, y1 + h // 2
-    #     cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
-    #
-    #     if limits[0] < cx < limits[2] and limits[1] - 15 < cy < limits[1] + 15:
-    #         if totalCount.count(id) == 0:
-    #             totalCount.append(id)
-    #             cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 5)
-
-    # cvzone.putTextRect(img, f' Count: {len(totalCount)}', (50, 50))
-    # cv2.putText(img, str(len(totalCount)), (255, 100), cv2.FONT_HERSHEY_PLAIN, 5, (50, 50, 255), 8)
-
-    # cv2.imshow("Image", img)
-    # cv2.waitKey(0)
-    # if cv2.waitKey(1) == ord("q"):
-    #     break
 cap.release()
 cv2.destroyWindow()
